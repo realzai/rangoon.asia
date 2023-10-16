82e605e0e1845bdd0ff925f79a40a0d2b449f3ae
Zai <zaibutcooler@icloud.com>
2023-10-16 21:14:33 +0630

    feat: typography — IM Fell + UnifrakturMaguntia + Playfair, paper grain

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main className="mx-auto max-w-broadsheet px-6 py-24">
-      <h1 className="text-4xl">The Rangoon Gazette</h1>
-      <p className="mt-4">A vintage broadsheet — under the press.</p>
+      <h1 className="font-blackletter text-7xl">The Rangoon Gazette</h1>
+      <p className="mt-4 font-serif italic">
+        A vintage broadsheet — under the press.
+      </p>
     </main>
   )
 }
